53be1457beb6a942fd4b8ca112ee54f445c6c402
Rian01048 <riansouzasilva247@gmail.com>
2026-06-26 20:33:11 -0300

    Add files via upload

---
+++ projeto2/projeto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("===== Verificação de idade =====");
+        Console.WriteLine("===== Verifique 5 idades =====");
+        Console.WriteLine("\n");
+        for (int count = 0; count < 5; count++)
+        {
+            Console.Write($"Digite a {count + 1}° idade: ");
+            byte idade = byte.Parse(Console.ReadLine());
+            if (idade >= 18)
+            {
+                Console.WriteLine($"\nIdade: {idade}");
+                Console.WriteLine("===== Maior de idade =====\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nIdade: {idade}");
+                Console.WriteLine("===== Menor de Idade =====\n");
+            }
+        }
+    }
+}
---
+++ projeto3/projeto3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("===== Verificar nota do aluno =====");
+        Console.Write("===== Digite as 3 notas do aluno =====\n");
+        double[] notas = new double[3];
+
+        for (int count = 0; count < 3; count++)
+        {
+            Console.Write($"\nDigite a {count+1} nota: ");
+            notas[count] = double.Parse( Console.ReadLine());
+        }
+
+        double soma = notas[0] + notas[1] + notas[2];
+        double media = soma / 3;
+        Console.WriteLine($"Média: {media}");
+        if (media >= 7)
+        {
+            Console.WriteLine("Aprovado!!");
+        }
+        else if (media >= 5 && media <= 6.9)
+        {
+            Console.WriteLine("Recuperação");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado!!");
+        }
+            
+    }
+}
